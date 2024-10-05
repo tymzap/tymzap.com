88d2624d15fcba124147a6970a2741bc7f7e0756
tymzap <tymzap@gmail.com>
2024-10-06 01:33:44 +0200

    feat: implement Hero component

---
+++ src/app/Hero/Hero.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+
+export const wrapper = style({
+  maxWidth: 700,
+  marginBottom: 50,
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-import { Text } from "~/components/Text";
-import { Heading } from "~/components/Heading";
-
 import { LatestArticles } from "./LatestArticles";
+import { Hero } from "./Hero";
 
 export default function Home() {
   return (
     <>
-      <Heading level={1}>Heading 1</Heading>
-      <Text>
-        From which we spring invent the universe corpus callosum the only home
-        we ve ever known decipherment dispassionate extraterrestrial observer.
-        Venture vanquish the impossible vastness is bearable only through love
-        not a sunrise but a galaxyrise the carbon in our apple pies
-        extraordinary claims require extraordinary evidence.
-      </Text>
+      <Hero />
       <LatestArticles />
     </>
   );
