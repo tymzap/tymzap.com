fcbd58d03d22bb812f3d24e06953667831838ef6
tymzap <tymzap@gmail.com>
2024-10-09 19:30:12 +0200

    refactor: extract generateStaticParams to separate file

---
+++ src/app/blog/[slug]/generateStaticParams.ts
@@ -0,0 +1,14 @@
+import path from "node:path";
+import fs from "node:fs";
+
+export function generateStaticParams() {
+  const articlesDirectory = path.resolve("./content");
+
+  const articleFilenames = fs
+    .readdirSync(articlesDirectory)
+    .map((filename) => filename.replace(".mdx", ""));
+
+  return articleFilenames.map((filename) => ({
+    slug: filename,
+  }));
+}
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,10 @@
-import path from "node:path";
-import fs from "node:fs";
-
 import { loadArticleFromSlug } from "~/lib/loadArticleFromSlug";
 import { LongContentWrapper } from "~/components/LongContentWrapper";
 
 import { BlogArticleHeader } from "./BlogArticleHeader";
 import { BlogArticleImage } from "./BlogArticleImage";
 import { AuthorBio } from "./AuthorBio";
+import { generateStaticParams } from "./generateStaticParams";
 
 type BlogArticleProps = {
   params: {
@@ -29,18 +27,6 @@ export default async function BlogArticle({ params }: BlogArticleProps) {
       <AuthorBio />
     </LongContentWrapper>
   );
-}
-
-export function generateStaticParams() {
-  const articlesDirectory = path.resolve("./content");
-
-  const articleFilenames = fs
-    .readdirSync(articlesDirectory)
-    .map((filename) => filename.replace(".mdx", ""));
-
-  return articleFilenames.map((filename) => ({
-    slug: filename,
-  }));
 }
 
 export const dynamicParams = false;
