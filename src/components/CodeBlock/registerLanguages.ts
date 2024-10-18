aeedd8a026431744981bd6c71ec9391eaa57c092
tymzap <tymzap@gmail.com>
2024-10-18 18:14:47 +0200

    content: publish Guide to advanced data serialization in NestJS article

--- src/components/CodeBlock/registerLanguages.ts
+++ src/components/CodeBlock/registerLanguages.ts
@@ -1,4 +1,5 @@
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import typescript from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 import css from "react-syntax-highlighter/dist/esm/languages/prism/css";
 import scss from "react-syntax-highlighter/dist/esm/languages/prism/scss";
@@ -6,3 +7,4 @@ import scss from "react-syntax-highlighter/dist/esm/languages/prism/scss";
 SyntaxHighlighter.registerLanguage("jsx", jsx);
 SyntaxHighlighter.registerLanguage("jsx", css);
 SyntaxHighlighter.registerLanguage("jsx", scss);
+SyntaxHighlighter.registerLanguage("jsx", typescript);
