a14c9d3771fcf6624e9aa59ca60ec055483f65b5
tymzap <tymzap@gmail.com>
2024-10-18 18:43:33 +0200

    refactor: use proper names for registering languages

--- src/components/CodeBlock/registerLanguages.ts
+++ src/components/CodeBlock/registerLanguages.ts
@@ -1,10 +1,12 @@
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
 import typescript from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 import css from "react-syntax-highlighter/dist/esm/languages/prism/css";
 import scss from "react-syntax-highlighter/dist/esm/languages/prism/scss";
 
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("javascript", javascript);
 SyntaxHighlighter.registerLanguage("jsx", jsx);
-SyntaxHighlighter.registerLanguage("jsx", css);
-SyntaxHighlighter.registerLanguage("jsx", scss);
-SyntaxHighlighter.registerLanguage("jsx", typescript);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("scss", scss);
