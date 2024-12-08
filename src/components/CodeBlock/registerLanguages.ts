a19f813f89c8342a5abc2470ae8a108db52686ad
tymzap <tymzap@gmail.com>
2024-12-08 02:54:47 +0100

    content: publish Stop bots from submitting your forms using a honeypot field article

--- src/components/CodeBlock/registerLanguages.ts
+++ src/components/CodeBlock/registerLanguages.ts
@@ -2,11 +2,13 @@ import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import javascript from "react-syntax-highlighter/dist/esm/languages/prism/javascript";
 import typescript from "react-syntax-highlighter/dist/esm/languages/prism/typescript";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
+import tsx from "react-syntax-highlighter/dist/esm/languages/prism/tsx";
 import css from "react-syntax-highlighter/dist/esm/languages/prism/css";
 import scss from "react-syntax-highlighter/dist/esm/languages/prism/scss";
 
 SyntaxHighlighter.registerLanguage("typescript", typescript);
 SyntaxHighlighter.registerLanguage("javascript", javascript);
 SyntaxHighlighter.registerLanguage("jsx", jsx);
+SyntaxHighlighter.registerLanguage("tsx", tsx);
 SyntaxHighlighter.registerLanguage("css", css);
 SyntaxHighlighter.registerLanguage("scss", scss);
