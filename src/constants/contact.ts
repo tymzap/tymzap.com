21b3b769c696a435af3691453487f2d7bbd147eb
tymzap <tymzap@gmail.com>
2024-12-18 20:08:18 +0100

    content: update LinkedIn link

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,5 +1,5 @@
 export const CONTACT_URLS = {
-  LINKED_IN: "https://www.linkedin.com/in/tymek-zapa%C5%82a-20719818b/",
+  LINKED_IN: "https://www.linkedin.com/in/tymzap",
   X: "https://x.com/tymzap",
   GITHUB: "https://github.com/tymzap",
   EMAIL: "mailto:tymzap@gmail.com",
