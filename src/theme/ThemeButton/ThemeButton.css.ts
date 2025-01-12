39d03710b39eed80a8f9c74679ce8c9e4817788b
tymzap <tymzap@gmail.com>
2025-01-12 17:37:19 +0100

    style: remove ThemeButton outline only on desktop

--- src/theme/ThemeButton/ThemeButton.css.ts
+++ src/theme/ThemeButton/ThemeButton.css.ts
@@ -3,19 +3,20 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "~/styles/themes/vars.css";
 import { mediaQueries } from "~/styles/mediaQueries";
 
-const buttonSize = 35;
+const buttonSize = 45;
 
 export const button = style({
   color: vars.colors.text,
   fontSize: 14,
   border: 0,
   cursor: "pointer",
+  padding: 13,
   width: buttonSize,
   height: buttonSize,
-  outline: "none",
   background: "transparent",
   "@media": {
     [mediaQueries.laptopOrBigger]: {
+      outline: "none",
       selectors: {
         "&:hover": {
           opacity: 0.65,
