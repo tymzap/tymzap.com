46a2b9bdc263d616b2469cf5adadc3f21d91aa8e
tymzap <tymzap@gmail.com>
2025-05-10 10:12:46 +0200

    refactor: move createSubscriber and fetchFromMailerLiteApi functions to newsletter directory

--- src/newsletter/createSubscriber.ts
+++ src/newsletter/createSubscriber.ts
@@ -1,5 +1,6 @@
-import { fetchFromMailerLiteApi } from "~/lib/fetchFromMailerLiteApi";
 import { SERVER_ENV } from "~/config/env/server";
+
+import { fetchFromMailerLiteApi } from "~/lib/fetchFromMailerLiteApi";
 
 export function createSubscriber(email: string) {
   return fetchFromMailerLiteApi("subscribers", {
