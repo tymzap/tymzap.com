f5a0a69260b09ed67776a1c3c9b8823b06d71035
tymzap <tymzap@gmail.com>
2025-05-10 23:08:44 +0200

    chore: add Plausible env variables

--- src/config/env/server.ts
+++ src/config/env/server.ts
@@ -2,6 +2,9 @@ import * as yup from "yup";
 
 export type ServerEnv = {
   API_TOKEN_SECRET: string;
+  PLAUSIBLE_SITE_ID: string;
+  PLAUSIBLE_API_URL: string;
+  PLAUSIBLE_API_KEY: string;
   MAILER_LITE_API_URL: string;
   MAILER_LITE_API_TOKEN: string;
   MAILER_LITE_GROUP_ID: string;
@@ -10,6 +13,9 @@ export type ServerEnv = {
 
 const schema: yup.Schema<ServerEnv> = yup.object({
   API_TOKEN_SECRET: yup.string().required(),
+  PLAUSIBLE_SITE_ID: yup.string().required(),
+  PLAUSIBLE_API_URL: yup.string().required(),
+  PLAUSIBLE_API_KEY: yup.string().required(),
   MAILER_LITE_API_URL: yup.string().required(),
   MAILER_LITE_API_TOKEN: yup.string().required(),
   MAILER_LITE_GROUP_ID: yup.string().required(),
